6368e7288175d715e9b7ae393ebdc04deb20720d
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2023-07-25 18:44:49 -0400

    Added product list page and route

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
 import Container from "@/components/ui/Container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
   const billboard = await getBillboard("280368dc-4dc6-4c7b-9581-95635e72e7a6");
+  const products = await getProducts({ isFeatured: true });
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
---
+++ components/ui/IconButton.tsx
@@ -0,0 +1,9 @@
+const IconButton = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default IconButton;
