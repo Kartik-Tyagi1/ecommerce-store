06b7730fae3802d7d571d652eeaccc26e20f1c10
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2023-07-26 14:27:18 -0400

    Finished up product card

--- components/ui/IconButton.tsx
+++ components/ui/IconButton.tsx
@@ -1,8 +1,22 @@
-const IconButton = () => {
+import { cn } from "@/lib/utils";
+import { MouseEventHandler } from "react";
+
+interface IconButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  icon: React.ReactElement;
+  classname?: string;
+}
+
+const IconButton : React.FC<IconButtonProps> = ({
+  onClick, icon, classname
+}) => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <button
+      onClick={onClick}
+      className={cn("rounded-full flex-items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition", classname)}
+    >
+      {icon}
+    </button>
   );
 };
 
